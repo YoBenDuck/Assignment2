2a12f206763143bc7a7736dcd396c05a5e53c03f
Ben <YoBenDuck@gmail.com>
2016-06-20 23:14:49 -0700

    Finalized assignment2

--- scriptpart2.js
+++ scriptpart2.js
@@ -199,7 +199,45 @@ for (var i = 1; i < 100; i+=1) {
 */
 
 //PART4 - The “Hitchhiker’s Guide to the Galaxy” Game 
+/*
+var start = confirm('Greetings! Are you ready to play The Hitchkiker\'s Guide to the Galaxy');
 
+if (start == true) {
+	alert('Awesome, our hero is waiting!');
+} else {
+	alert('Too bad, we\’re going to play anyway because our hero desperately needs your help!');
+}
+
+alert('You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall…');
+var direction = prompt('Which direction would you like to head (please enter forward, left, or right).');
+
+switch (direction.toLowerCase()) {
+	case 'forward' :
+		alert('You walk about 100 yards and safely make your way out of the cave.');
+		break;
+	case 'left' :
+		alert('Your thirst has gotten the better of you. You trip on a rock, hit your head, and fall into a pool of water and drown.');
+		break;
+	case 'right' :
+		alert('You found the gold! You walk into a small room and see thousands of gold coins, jewels, chalices, and more. You jump into the pile of gold in celebration and immediately a hidden door slams down and traps you in the room forever.');
+		break;
+	default :
+		alert('The ghost of Captain Chingadera has condemned you to eternal damnation and you shall now burn in the hot excoriation for lifeless lowlifes for not choosing the correct option….loser.');
+}
+
+var rating = prompt('Please rate this game on a scale of 1 to 10');
+if (rating < 1 || rating > 10 ) {
+	rating = 10;
+}  
+if (rating <= 5) {
+	alert('Whatever, you weren’t very good at this game anyway...Schmuck!');
+} else {
+	alert
+('Thank you, we will continue to make improvements to the game!');
+}
+
+console.log(rating);
+*/
 
 //ASSIGNMENT2 PART5 - The “Coin Flip” Game 
 //*** Ben's COIN FLIP GAME STEPS 1 thru 8***
